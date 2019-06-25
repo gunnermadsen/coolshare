ec80478d62a5a5babd1dfbbb4b732aaab807fa6f
gunnermadsen <gunner.madsen@outlook.com>
2019-06-25 15:39:23 -0700

    Fixed errors causing unsuccessful build

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/dist/CoolShare'));
+
 app.get('/*', (request, response) => {
     response.sendFile(path.join(__dirname + '/dist/CoolShare/index.html'));
 });
