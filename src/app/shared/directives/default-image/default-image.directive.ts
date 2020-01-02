006e717d707424e3627ad1de941caddb328ffca5
gunnermadsen <gunner.madsen@outlook.com>
2020-01-02 15:13:03 -0800

    Updated main.layout.component, and entity-info.component styles. Updated backup server for obtaining thumbnail images for files in default-image.directive

--- src/app/shared/directives/default-image/default-image.directive.ts
+++ src/app/shared/directives/default-image/default-image.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, isDevMode } from '@angular/core';
+import { Directive, Input, isDevMode } from '@angular/core'
 
 @Directive({
   selector: 'img[src]',
@@ -8,20 +8,25 @@ import { Directive, Input, isDevMode } from '@angular/core';
   }
 })
 export class DefaultImageDirective {
+
   @Input() 
-  public src: string;
+  public src: string
 
   @Input() 
   public filename: string
-  public defaultImg: string = '/icons/default.svg';
+
+  public defaultImg: string = '/icons/default.svg'
   private server: string
+  private onErrorServer: string
 
   constructor() {
     this.server = isDevMode() ? 'http://localhost:3000' : 'https://portfolioapis.herokuapp.com'
+    this.onErrorServer = isDevMode() ? 'http://localhost:4200' : 'https://coolshare.herokuapp.com'
   }
+
   public onError() {
-    const extension = this.filename.split('.').pop() || 'default'
-    this.src = `${this.server}/icons/${extension}.svg`
+    const extension = this.filename.split('.').pop()
+    this.src = `${this.onErrorServer}/assets/icons/${extension}.svg`
   }
   public checkPath(src: string) {
     return src ? src : `${this.server}${this.defaultImg}`
